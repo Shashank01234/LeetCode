145f4d46c5d13ea6f08ffd114cff0fa06f25bde0
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-06-15 02:12:37 +0530

    Time: 1 ms (99.41%) | Memory: 61.8 MB (11.28%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
@@ -1,9 +1,25 @@
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        final int xors1 = Arrays.stream(nums1).reduce((a, b) -> a ^ b).getAsInt();
-        final int xors2 = Arrays.stream(nums2).reduce((a, b) -> a ^ b).getAsInt();
-        return (nums1.length % 2 * xors2) ^ (nums2.length % 2 * xors1);
+        int xor1 = 0, xor2 = 0;
+
+        for (int num : nums1) {
+            xor1 ^= num;
+        }
+
+        for (int num : nums2) {
+            xor2 ^= num;
+        }
+
+        int result = 0;
+
+        if (nums2.length % 2 == 1) {
+            result ^= xor1;
+        }
+
+        if (nums1.length % 2 == 1) {
+            result ^= xor2;
+        }
+
+        return result;
     }
 }
-
-
