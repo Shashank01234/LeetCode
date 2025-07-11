027a1b2bb6f1da42cf61068805845f97a35762f1
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-07-11 16:43:15 +0530

    Time: 2 ms (86.26%) | Memory: 48.1 MB (69.71%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -2,14 +2,12 @@ class Solution {
     public void reorderList(ListNode head) {
         if (head == null) return;
 
-        // Step 1: Find the middle of the list
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // Step 2: Reverse the second half of the list
         ListNode second = slow.next;
         slow.next = null;
         ListNode node = null;
@@ -21,7 +19,6 @@ class Solution {
             second = temp;
         }
 
-        // Step 3: Merge the two halves
         ListNode first = head;
         second = node;
 
