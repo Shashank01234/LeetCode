865099c361572000da59aa2332659e90155d7d0c
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-08-18 22:47:55 +0530

    Time: 2 ms (98.92%) | Memory: 45 MB (47.02%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,11 +6,8 @@ class Solution {
             if(map.containsKey(target - nums[i])){
                 return new int[] {map.get(target - nums[i]), i};
             }
-            else{
-                map.put(nums[i], i);
-            }
+            map.put(nums[i], i);   
         }
-
         return new int[] {};
     }
 }
