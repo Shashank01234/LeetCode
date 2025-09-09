395f1b64f57f41af66ea6540cf7bddf98baf33ec
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-09-10 02:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (18.18%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -8,8 +8,8 @@ class Solution {
             int mid = left + (right-left)/2;
 
             if(letters[mid]>target) right = mid-1;
-            else if(letters[mid]<target) left = mid+1;
             else left = mid+1;
+
         }
 
         return letters[left % letters.length];
