5f8e35756b7f7ae507038064f49a586f3885d20c
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-11-09 18:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.68%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -3,14 +3,12 @@ class Solution {
         int result=0;
 
         while(n1!=0 && n2!=0){
-            if(n1>n2){
-                n1-=n2;
-            }
-            else{
-                n2-=n1;
-            }
+            result += n1/n2;
+            n1 %= n2;
 
-            result++;
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
         }
 
         return result;
