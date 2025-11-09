cf17693cd77c5672cd93876eb20170a81cd94e6d
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-11-09 18:30:36 +0530

    Time: 3 ms (14.25%) | Memory: 42.3 MB (11.81%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -1,25 +1,18 @@
 class Solution {
-    int n1;
-    int n2;
-    public int countOperations(int num1, int num2) {
-        if(num1 == 0 || num2 == 0) return 0;
-        n1 = num1;
-        n2 = num2;
+    public int countOperations(int n1, int n2) {
+        int result=0;
 
-        return operations(1);
-    }
+        while(n1!=0 && n2!=0){
+            if(n1>n2){
+                n1-=n2;
+            }
+            else{
+                n2-=n1;
+            }
 
-
-    private int operations(int count){
-        if(n1>n2){
-            n1-=n2;
-        }
-        else{
-            n2-=n1;
+            result++;
         }
 
-        if(n1==0 || n2==0) return count;
-
-        return operations(count+1);
+        return result;
     }
 }
