c0a5bda83a4ae97502937548cc123ce6423c1c0c
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-11-17 17:13:22 +0530

    Time: 1 ms (99.71%) | Memory: 65.5 MB (15.14%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        int count=-1;
-        for(int num: nums){
-            if(num==1 && (count==-1 || count>=k)){
-                count=0;
-                continue;
-            }
-            else if(num==1 && (count!=-1 || count<k)){
-                return false;
-            }
-
-            else if(count!=-1){
-                count++;
+        int lastOccured = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (lastOccured != -1) {
+                    int gap = i - lastOccured - 1;
+                    if (gap < k) return false;
+                }
+                lastOccured = i;
             }
         }
-
         return true;
     }
 }
