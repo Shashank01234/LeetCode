e3f074b7a42107ff6e62382a10a4c918d5559330
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2025-11-29 03:15:04 +0530

    Time: 31 ms (43.62%) | Memory: 92.2 MB (20.21%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -1,7 +1,7 @@
 class Solution {
     private int count=1;
+    private List<List<Integer>> adj = new ArrayList<>();
     public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
-        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0; i<n; i++){
             adj.add(new ArrayList<>());
         }
@@ -11,18 +11,18 @@ class Solution {
             adj.get(e[1]).add(e[0]);
         }
 
-        dfs(0, -1, values, k, adj);
+        dfs(0, -1, values, k);
 
         return count;
     }
 
-    private long dfs(int u, int p, int[] values, int k, List<List<Integer>> adj){
+    private long dfs(int u, int p, int[] values, int k){
         long sum = values[u];
 
         for(int v: adj.get(u)){
             if(v==p) continue;
 
-            long sub = dfs(v, u, values, k, adj);
+            long sub = dfs(v, u, values, k);
             if(sub%k==0) count++;
             else sum+=sub;
         }
