565ed794786808047c9e76f7b4a85792f4ab78fe
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-01-01 12:22:00 +0530

    Time: 4 ms (3.57%) | Memory: 87.9 MB (7.59%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -5,13 +5,13 @@ class Solution {
         long a=0L;
 
         for(int i=1; i<prices.length; i++){
+            descend[i]+=descend[i-1];
             if(prices[i]+1==prices[i-1]){
                 a++;
-                descend[i]+=descend[i-1]+a;
+                descend[i]+=a;
             }
             else{
                 a=0;
-                descend[i]+=descend[i-1];
             }
         }
 
