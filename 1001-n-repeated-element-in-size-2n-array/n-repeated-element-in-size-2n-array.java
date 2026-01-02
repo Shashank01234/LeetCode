a5e79864f219895ed70f5c59687ceb12c7157393
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-01-02 14:26:39 +0530

    Time: 12 ms (34.73%) | Memory: 48 MB (6.11%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,17 +1,29 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int n = nums.length;
+        int n = nums.length/2;
+        Arrays.sort(nums);
 
-        Map<Integer, Integer> freq = new HashMap<>();
-        for(int num: nums){
-            freq.put(num, freq.getOrDefault(num, 0)+1);
-        }
+        int prev=nums[0];
+        int count=1;
+        for(int i=1; i<n*2; i++){
+            if(nums[i]==prev){
+                count++;
+            }
+            else{
+                if(count==n){
+                    return prev;
+                }
+                else{
+                    count=1;
+                    prev=nums[i];
+                }
+            }
 
-        for(Map.Entry<Integer, Integer> entry: freq.entrySet()){
-            if(entry.getValue()==n/2){
-                return entry.getKey();
+            if(count==n){
+                return prev;
             }
         }
+
         return -1;
     }
 }
