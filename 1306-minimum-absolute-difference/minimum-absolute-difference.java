3e964f4148dc0c987054370a23dda25ba1a47fce
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-01-26 21:50:00 +0530

    Time: 20 ms (97.68%) | Memory: 64.2 MB (13.24%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -6,17 +6,12 @@ class Solution {
         int minDif = Integer.MAX_VALUE;
         int p=1;
         while(p<arr.length){
-            if(arr[p] - arr[p-1] < minDif){
-                minDif = arr[p] - arr[p-1];
-                result.clear();
-
-                List<Integer> list = new ArrayList<>();
-                list.add(arr[p-1]);
-                list.add(arr[p]);
-                result.add(list);
-            }
-
-            else if(arr[p] - arr[p-1] == minDif){
+            if(arr[p] - arr[p-1] <= minDif){
+                if(arr[p] - arr[p-1] < minDif){
+                    minDif = arr[p] - arr[p-1];
+                    result.clear();
+                }
+                
                 List<Integer> list = new ArrayList<>();
                 list.add(arr[p-1]);
                 list.add(arr[p]);
