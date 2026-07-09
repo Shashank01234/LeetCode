c73587e27f724fc5d4fc3e3f17c94248868d499c
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-07-09 16:27:02 +0530

    Time: 2 ms (94.15%) | Memory: 65.2 MB (70.33%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if(nums.length == 0){
-            return new int[0];
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int[] pPro = new int[n];
+        pPro[0] = nums[0];
+        for(int i=1; i<n; i++){
+            pPro[i] = pPro[i-1] * nums[i];
         }
 
-        int pre=1, post=1;
-        int[] result = new int[nums.length];
-        Arrays.fill(result, 1);
-        for(int i=1; i<nums.length; i++){
-            pre*=nums[i-1];
-            result[i]=pre;
+        int[] sPro = new int[n];
+        sPro[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            sPro[i] = sPro[i+1] * nums[i]; 
         }
 
-        for(int i=nums.length-2; i>=0; i--){
-            post*=nums[i+1];
-            result[i]*=post;
+        for(int i=0; i<n; i++){
+            if(i==0){
+                answer[i] = sPro[i+1];
+            }
+
+            else if(i==n-1){
+                answer[i] = pPro[i-1];
+            }
+
+            else{
+                answer[i] = sPro[i+1] * pPro[i-1];
+            }
         }
 
-        return result;
+        return answer;
     }
 }
