ab72c08114981f3f2fbce678bf1f4b92a11dec98
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-07-09 18:04:40 +0530

    Time: 2 ms (70.79%) | Memory: 47.1 MB (72.57%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,38 +1,52 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int res = Integer.MIN_VALUE;
 
-        int pre = 1;
-        int post = 1;
+        int max = nums[0];
+        int min = nums[0];
 
-        int i=0, j=n-1;
-        while(i<n && j>=0){
-            if(nums[i] == 0){
-                i++;
-                pre = i<n ? 1:0;
+        int res = max;
 
-                res = Math.max(res, 0);    
-            }
+        for(int i=1; i<n; i++){
+            int cur = nums[i];
+            int temp = Math.max(cur, Math.max(cur*min, cur*max));
+            min = Math.min(cur, Math.min(cur*min, cur*max));
+            max = temp;
 
-            if(nums[j] == 0){
-                j--;
-                post = j>=0 ? 1:0;
-
-                res = Math.max(res, 0);
-            }
-
-            if(i<n){
-                pre *= nums[i++];
-            }
-
-            if(j>=0){
-                post *= nums[j--];
-            }
-
-
-            res = Math.max(res, Math.max(pre, post));
+            res = Math.max(res, max);
         }
+        // int res = Integer.MIN_VALUE;
+
+        // int pre = 1;
+        // int post = 1;
+
+        // int i=0, j=n-1;
+        // while(i<n && j>=0){
+        //     if(nums[i] == 0){
+        //         i++;
+        //         pre = i<n ? 1:0;
+
+        //         res = Math.max(res, 0);    
+        //     }
+
+        //     if(nums[j] == 0){
+        //         j--;
+        //         post = j>=0 ? 1:0;
+
+        //         res = Math.max(res, 0);
+        //     }
+
+        //     if(i<n){
+        //         pre *= nums[i++];
+        //     }
+
+        //     if(j>=0){
+        //         post *= nums[j--];
+        //     }
+
+
+        //     res = Math.max(res, Math.max(pre, post));
+        // }
 
         return res;
     }
