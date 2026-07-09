5a333de1a25ab711a6c482dfac204f3e0e96f7c1
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-07-09 16:35:57 +0530

    Time: 2 ms (94.15%) | Memory: 72.2 MB (27.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,31 +3,43 @@ class Solution {
         int n = nums.length;
         int[] answer = new int[n];
 
-        int[] pPro = new int[n];
-        pPro[0] = nums[0];
-        for(int i=1; i<n; i++){
-            pPro[i] = pPro[i-1] * nums[i];
-        }
-
-        int[] sPro = new int[n];
-        sPro[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            sPro[i] = sPro[i+1] * nums[i]; 
-        }
-
+        int pre = 1, post = 1;
+        // Arrays.fill(answer, 1);
         for(int i=0; i<n; i++){
-            if(i==0){
-                answer[i] = sPro[i+1];
-            }
-
-            else if(i==n-1){
-                answer[i] = pPro[i-1];
-            }
-
-            else{
-                answer[i] = sPro[i+1] * pPro[i-1];
-            }
+            answer[i] = pre;
+            pre *= nums[i];
         }
+
+        for(int i=n-1; i>=0; i--){
+            answer[i] *= post;
+            post *= nums[i];
+        }
+
+        // int[] pPro = new int[n];
+        // pPro[0] = nums[0];
+        // for(int i=1; i<n; i++){
+        //     pPro[i] = pPro[i-1] * nums[i];
+        // }
+
+        // int[] sPro = new int[n];
+        // sPro[n-1] = nums[n-1];
+        // for(int i=n-2; i>=0; i--){
+        //     sPro[i] = sPro[i+1] * nums[i]; 
+        // }
+
+        // for(int i=0; i<n; i++){
+        //     if(i==0){
+        //         answer[i] = sPro[i+1];
+        //     }
+
+        //     else if(i==n-1){
+        //         answer[i] = pPro[i-1];
+        //     }
+
+        //     else{
+        //         answer[i] = sPro[i+1] * pPro[i-1];
+        //     }
+        // }
 
         return answer;
     }
