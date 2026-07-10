6098e6bf0626ba545d8b702b4188a4d2169b84be
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-07-10 22:38:45 +0530

    Time: 1936 ms (5.01%) | Memory: 61 MB (5.85%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3)
-            return new ArrayList<>();
-
-        List<List<Integer>> ans = new ArrayList<>();
-
+        Set<List<Integer>> set = new HashSet<>();
+        
         Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            int target = 0 - nums[i];
+            int l=0, r=nums.length-1;
 
-        for (int i = 0; i + 2 < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            while(l<r){
+                if(l==i){
+                    l++;
+                    continue;
+                }
+                if(r==i){
+                    r--; 
+                    continue;
+                }
 
-            int l = i + 1;
-            int r = nums.length - 1;
-            while (l < r) {
-                final int sum = nums[i] + nums[l] + nums[r];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
-                    while (l < r && nums[l] == nums[l - 1])
-                        ++l;
-                    while (l < r && nums[r] == nums[r + 1])
-                        --r;
-                } else if (sum < 0) {
-                    ++l;
-                } else {
-                    --r;
+                if(nums[l] + nums[r] == target){
+                    List<Integer> list = new ArrayList<>(List.of(nums[i], nums[l], nums[r]));
+                    Collections.sort(list);
+
+                    set.add(list);
+                    l++;
+                    r--;
+                }
+
+                else if(nums[l] + nums[r] > target){
+                    r--;
+                }
+
+                else if(nums[l] + nums[r] < target){
+                    l++;
                 }
             }
         }
-
-        return ans;
+        
+        return new ArrayList<>(set);
     }
 }
