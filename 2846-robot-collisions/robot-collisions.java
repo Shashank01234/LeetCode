f0101dc8b1262217256357ae42d89e1ed84025af
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-07-20 04:24:40 +0530

    Time: 45 ms (50.64%) | Memory: 122.5 MB (38.05%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -1,34 +1,74 @@
 class Solution {
+    static class Robot{
+        public int position;
+        public int health;
+        public char direction;
+        public int order;
+        public Robot(int position, int health, char direction, int order){
+            this.position = position;
+            this.health = health;
+            this.direction = direction;
+            this.order = order;
+        }
+    }
+
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        ArrayList<Integer> ans = new ArrayList<>();
         int n = positions.length;
-        Integer[] order = new Integer[n];
-        for (int i = 0; i < n; i++) order[i] = i;
-        Arrays.sort(order, (a, b) -> positions[a] - positions[b]);
 
-        boolean[] dead = new boolean[n];
-        Deque<Integer> st = new ArrayDeque<>();
+        Robot[] robot = new Robot[n];
+        Stack<Robot> right = new Stack<>();
+        ArrayList<Robot> res = new ArrayList<>();
 
-        for (int i : order) {
-            if (directions.charAt(i) == 'R') {
-                st.push(i);
-            } else {
-                while (!st.isEmpty() && directions.charAt(st.peek()) == 'R') {
-                    int top = st.peek();
-                    if (healths[top] > healths[i]) {
-                        healths[top]--; dead[i] = true; break;
-                    } else if (healths[top] < healths[i]) {
-                        healths[i]--; dead[top] = true; st.pop();
-                    } else {
-                        dead[i] = dead[top] = true; st.pop(); break;
+        for(int i=0; i<n; i++){
+            robot[i] = new Robot(positions[i], healths[i], directions.charAt(i), i);
+        }
+
+        Arrays.sort(robot, Comparator.comparingInt(a -> a.position));
+
+        for(int i=0; i<n; i++){
+            if(robot[i].direction == 'R'){
+                right.push(robot[i]);
+            }
+            else{
+                if(right.isEmpty()){
+                    res.add(robot[i]);
+                }
+
+                while(!right.isEmpty()){
+                    Robot r = right.pop();
+
+                    if(r.health > robot[i].health){
+                        r.health--;
+                        right.push(r);
+                        break;
+                    }
+
+                    else if(r.health < robot[i].health){
+                        robot[i].health--;
+
+                        if(right.isEmpty()){
+                            res.add(robot[i]);
+                        }
+                    }
+                    
+                    else{
+                        break;
                     }
                 }
-                if (!dead[i]) st.push(i);
             }
         }
 
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            if (!dead[i]) res.add(healths[i]);
-        return res;
+        int size = res.size();
+        while(!right.isEmpty()){
+            res.add(size, right.pop());
+        }
+
+        Collections.sort(res, Comparator.comparingInt(a -> a.order));
+
+        for(Robot r: res){
+            ans.add(r.health);
+        }
+        return ans;
     }
 }
