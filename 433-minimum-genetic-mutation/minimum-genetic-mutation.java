ff80c70ffc7a2350e34e2cbc0f7a119c65fb3f0f
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-08-23 21:10:36 +0530

    Time: 11 ms (6.82%) | Memory: 44 MB (7.33%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,14 +1,15 @@
 class Solution {
     private int res=0;
+    private ArrayList<String> visited = new ArrayList<>();
     public int minMutation(String startGene, String endGene, String[] bank) {
         Queue<String> queue = new LinkedList<>();
         queue.offer(startGene);
 
-        bfs(queue, endGene, bank, new ArrayList<>());
+        bfs(queue, endGene, bank);
         return res;
     }
 
-    private void bfs(Queue<String> queue, String endGene, String[] bank, ArrayList<String> visited) {
+    private void bfs(Queue<String> queue, String endGene, String[] bank) {
         if(queue.isEmpty()) {
             res = -1;
             return;
@@ -24,16 +25,16 @@ class Solution {
             for(int i=0; i<8; i++) {
                 char ch = sb.charAt(i);
                 sb.setCharAt(i, 'A');
-                checkString(sb.toString(), bank, next, visited);
+                checkString(sb.toString(), bank, next);
 
                 sb.setCharAt(i, 'C');
-                checkString(sb.toString(), bank, next, visited);
+                checkString(sb.toString(), bank, next);
 
                 sb.setCharAt(i, 'G');
-                checkString(sb.toString(), bank, next, visited);
+                checkString(sb.toString(), bank, next);
 
                 sb.setCharAt(i, 'T');
-                checkString(sb.toString(), bank, next, visited);    
+                checkString(sb.toString(), bank, next);    
 
                 sb.setCharAt(i, ch);            
             }
@@ -43,11 +44,11 @@ class Solution {
 
         res++;
 
-        bfs(next, endGene, bank, visited);
+        bfs(next, endGene, bank);
         return;
     }
 
-    private void checkString(String str, String[] bank, Queue<String> queue, ArrayList<String> visited) {
+    private void checkString(String str, String[] bank, Queue<String> queue) {
         for(String s: bank) {
             if(str.equals(s)) {
                 if(!visited.contains(str)) {
