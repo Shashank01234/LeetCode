6fed6b2ccc1b118cca0eed4cec2d740cad88842d
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-08-25 01:58:00 +0530

    Time: 6 ms (68.75%) | Memory: 48.6 MB (5.63%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -2,29 +2,29 @@ class Solution {
     private int[][] dir = {{0,-1}, {-1,0}, {0,1}, {1,0}};
     private int res=0;
     private boolean[][] visited;
+    private Queue<int[]> queue = new LinkedList<>();
     public int nearestExit(char[][] maze, int[] entrance) {
         int m = maze.length;
         int n = maze[0].length;
 
         visited = new boolean[m][n];
-        Queue<int[]> queue = new LinkedList<>();
         queue.offer(entrance);
 
         visited[entrance[0]][entrance[1]] = true;
-        bfs(queue, maze);
+        bfs(maze);
 
         return res;
     }
 
-    private void bfs(Queue<int[]> queue, char[][] maze) {
+    private void bfs(char[][] maze) {
         if(queue.isEmpty()) {
             res = -1;
             return;
         }
 
-        Queue<int[]> next = new LinkedList<>();
+        int q = queue.size();
 
-        while(!queue.isEmpty()) {
+        for(int i=0; i<q; i++) {
             int[] pos = queue.poll();
 
             if(pos[0] == 0 || pos[0] == maze.length-1 || pos[1] == 0 || pos[1] == maze[0].length-1) {
@@ -36,20 +36,20 @@ class Solution {
             for(int[] d: dir) {
                 int[] newPos = {pos[0]+d[0], pos[1]+d[1]};
 
-                checkPos(newPos, maze, next);
+                checkPos(newPos, maze);
             }
         }
         
         res++;
-        bfs(next, maze);
+        bfs(maze);
     }
 
-    private void checkPos(int[] pos, char[][] maze, Queue<int[]> next) {
+    private void checkPos(int[] pos, char[][] maze) {
         if(pos[0] < 0 || pos[0] >= maze.length || pos[1] < 0 || pos[1] >= maze[0].length || maze[pos[0]][pos[1]] == '+' || visited[pos[0]][pos[1]]) {
             return;
         }
 
-        next.offer(pos);
+        queue.offer(pos);
        
         visited[pos[0]][pos[1]] = true;
     }
