00f2f7d4b55d86d3422d90c98c187a766e6b071b
Shashank Pandey <143015355+Shashank01234@users.noreply.github.com>
2026-08-25 02:20:43 +0530

    Time: 6 ms (68.75%) | Memory: 48.4 MB (24.07%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -1,22 +1,22 @@
 class Solution {
     private int[][] dir = {{0,-1}, {-1,0}, {0,1}, {1,0}};
     private int res=0;
-    private boolean[][] visited;
-    private Queue<int[]> queue = new LinkedList<>();
     public int nearestExit(char[][] maze, int[] entrance) {
+
         int m = maze.length;
         int n = maze[0].length;
+        boolean[][] visited = new boolean[m][n];
 
-        visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
         queue.offer(entrance);
 
         visited[entrance[0]][entrance[1]] = true;
-        bfs(maze);
+        bfs(queue, maze, visited);
 
         return res;
     }
 
-    private void bfs(char[][] maze) {
+    private void bfs(Queue<int[]> queue, char[][] maze, boolean[][] visited) {
         if(queue.isEmpty()) {
             res = -1;
             return;
@@ -36,15 +36,15 @@ class Solution {
             for(int[] d: dir) {
                 int[] newPos = {pos[0]+d[0], pos[1]+d[1]};
 
-                checkPos(newPos, maze);
+                checkPos(queue, newPos, maze, visited);
             }
         }
         
         res++;
-        bfs(maze);
+        bfs(queue, maze, visited);
     }
 
-    private void checkPos(int[] pos, char[][] maze) {
+    private void checkPos(Queue<int[]> queue, int[] pos, char[][] maze, boolean[][] visited) {
         if(pos[0] < 0 || pos[0] >= maze.length || pos[1] < 0 || pos[1] >= maze[0].length || maze[pos[0]][pos[1]] == '+' || visited[pos[0]][pos[1]]) {
             return;
         }
